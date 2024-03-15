9440db592b6dc931827c2c43d2fe24938f921e9a
Katrina Connors <koktakatrina@gmail.com>
2024-03-15 15:35:46 -0700

    flexbox styling

--- src/App.tsx
+++ src/App.tsx
@@ -58,221 +58,239 @@ function App() {
           your guess.
         </Alert>
       ) : null}
-      <Card
-        style={{
-          width: "180pt",
-        }}
-      >
-        <CardBody>
-          <CardTitle tag="h4">How To Play</CardTitle>
-          <CardText>
-            -Guess the last name of the woman attributed with the shown quote
-            <br />
-            -You have 5 guesses until the woman's name is revealed <br />{" "}
-            -Letters that are green/underlined (in the Previous Guesses section
-            below) appear in the woman's name
-          </CardText>
-        </CardBody>
-      </Card>
-      <br />
-      <Card
-        style={{
-          width: "180pt",
-        }}
-      >
-        <CardBody>
-          <CardTitle tag="h4">Previous Guesses</CardTitle>
-          <Badge color="primary">
-            Guesses left: {5 - PreviousGuesses.length}{" "}
-          </Badge>{" "}
-          <br />
-          <CardText>
-            {/* https://stackoverflow.com/questions/1966476/how-can-i-process-each-letter-of-text-using-javascript */}
-            {PreviousGuesses.map((guess) => {
-              if (HistoricalWoman != null) {
-                const WomanName = HistoricalWoman.lastname.toUpperCase();
-                return (
-                  <div>
-                    {guess.split("").map((letter) => {
-                      if (WomanName.includes(letter)) {
-                        return (
-                          <div
-                            style={{
-                              display: "inline-block",
-                              color: "green",
-                              textDecorationLine: "underline",
-                            }}
-                          >
-                            {letter}
-                          </div>
-                        );
-                      } else {
-                        return (
-                          <div
-                            style={{ display: "inline-block", color: "black" }}
-                          >
-                            {letter}
-                          </div>
-                        );
-                      }
-                    })}
-                  </div>
-                );
-              }
-            })}
-          </CardText>
-        </CardBody>
-      </Card>
-      <br />
-      <div className="answervalue"> {AnswerValue}</div> <br />
-      {PreviousGuesses.length >= 5 ||
-      HistoricalWoman == null ||
-      HistoricalWoman.lastname.toUpperCase() ===
-        PreviousGuesses[PreviousGuesses.length - 1] ? null : (
-        <div>
-          <LetterButton
-            letter="Q"
-            onClick={() => UpdateAnswer([...AnswerValue, "Q"])}
-          />
-
-          <LetterButton
-            letter="W"
-            onClick={() => UpdateAnswer([...AnswerValue, "W"])}
-          />
-
-          <LetterButton
-            letter="E"
-            onClick={() => UpdateAnswer([...AnswerValue, "E"])}
-          />
-          <LetterButton
-            letter="R"
-            onClick={() => UpdateAnswer([...AnswerValue, "R"])}
-          />
-          <LetterButton
-            letter="T"
-            onClick={() => UpdateAnswer([...AnswerValue, "T"])}
-          />
-          <LetterButton
-            letter="Y"
-            onClick={() => UpdateAnswer([...AnswerValue, "Y"])}
-          />
-          <LetterButton
-            letter="U"
-            onClick={() => UpdateAnswer([...AnswerValue, "U"])}
-          />
-          <LetterButton
-            letter="I"
-            onClick={() => UpdateAnswer([...AnswerValue, "I"])}
-          />
-          <LetterButton
-            letter="O"
-            onClick={() => UpdateAnswer([...AnswerValue, "O"])}
-          />
-          <LetterButton
-            letter="P"
-            onClick={() => UpdateAnswer([...AnswerValue, "P"])}
-          />
-          <BackspaceButton
-            message="Backspace"
-            onClick={() => UpdateAnswer(AnswerValue.slice(0, -1))}
-          />
-          <br />
-          <LetterButton
-            letter="A"
-            onClick={() => UpdateAnswer([...AnswerValue, "A"])}
-          />
-          <LetterButton
-            letter="S"
-            onClick={() => UpdateAnswer([...AnswerValue, "S"])}
-          />
-          <LetterButton
-            letter="D"
-            onClick={() => UpdateAnswer([...AnswerValue, "D"])}
-          />
-          <LetterButton
-            letter="F"
-            onClick={() => UpdateAnswer([...AnswerValue, "F"])}
-          />
-          <LetterButton
-            letter="G"
-            onClick={() => UpdateAnswer([...AnswerValue, "G"])}
-          />
-
-          <LetterButton
-            letter="H"
-            onClick={() => UpdateAnswer([...AnswerValue, "H"])}
-          />
-          <LetterButton
-            letter="J"
-            onClick={() => UpdateAnswer([...AnswerValue, "J"])}
-          />
-
-          <LetterButton
-            letter="K"
-            onClick={() => UpdateAnswer([...AnswerValue, "K"])}
-          />
-
-          <LetterButton
-            letter="L"
-            onClick={() => UpdateAnswer([...AnswerValue, "L"])}
-          />
-          <LetterButton
-            letter="'"
-            onClick={() => UpdateAnswer([...AnswerValue, "'"])}
-          />
-          <br />
-          <LetterButton
-            letter="Z"
-            onClick={() => UpdateAnswer([...AnswerValue, "Z"])}
-          />
-          <LetterButton
-            letter="X"
-            onClick={() => UpdateAnswer([...AnswerValue, "X"])}
-          />
-          <LetterButton
-            letter="C"
-            onClick={() => UpdateAnswer([...AnswerValue, "C"])}
-          />
-          <LetterButton
-            letter="V"
-            onClick={() => UpdateAnswer([...AnswerValue, "V"])}
-          />
-
-          <LetterButton
-            letter="B"
-            onClick={() => UpdateAnswer([...AnswerValue, "B"])}
-          />
-          <LetterButton
-            letter="N"
-            onClick={() => UpdateAnswer([...AnswerValue, "N"])}
-          />
-          <LetterButton
-            letter="M"
-            onClick={() => UpdateAnswer([...AnswerValue, "M"])}
-          />
-          <LetterButton
-            letter="-"
-            onClick={() => UpdateAnswer([...AnswerValue, "-"])}
-          />
-
-          <br></br>
-
-          <GuessButton
-            message="Guess Name"
-            onClick={() => {
-              UpdateLengthAlert(false);
-              if (HistoricalWoman != null) {
-                if (AnswerValue.length == HistoricalWoman.lastname.length) {
-                  AddGuess([...PreviousGuesses, AnswerValue.join("")]);
-                  UpdateAnswer([]);
-                } else {
-                  UpdateLengthAlert(true);
-                }
-              }
+      <div className="cardkeyboard">
+        <div className="guidecards" style={{}}>
+          <Card
+            style={{
+              width: "180pt",
             }}
-          />
+          >
+            <CardBody>
+              <CardTitle tag="h4">How To Play</CardTitle>
+              <CardText>
+                -Guess the last name of the woman attributed with the shown
+                quote
+                <br />
+                -You have 5 guesses until the woman's name is revealed <br />{" "}
+                -Letters that are green/underlined (in the Previous Guesses
+                section below) appear in the woman's name
+              </CardText>
+            </CardBody>
+          </Card>
+          <br />
+          <Card
+            style={{
+              width: "180pt",
+            }}
+          >
+            <CardBody>
+              <CardTitle tag="h4">Previous Guesses</CardTitle>
+              <Badge color="primary">
+                Guesses left: {5 - PreviousGuesses.length}{" "}
+              </Badge>{" "}
+              <br />
+              <CardText>
+                {/* https://stackoverflow.com/questions/1966476/how-can-i-process-each-letter-of-text-using-javascript */}
+                {PreviousGuesses.map((guess) => {
+                  if (HistoricalWoman != null) {
+                    const WomanName = HistoricalWoman.lastname.toUpperCase();
+                    return (
+                      <div>
+                        {guess.split("").map((letter) => {
+                          if (WomanName.includes(letter)) {
+                            return (
+                              <div
+                                style={{
+                                  display: "inline-block",
+                                  color: "green",
+                                  textDecorationLine: "underline",
+                                }}
+                              >
+                                {letter}
+                              </div>
+                            );
+                          } else {
+                            return (
+                              <div
+                                style={{
+                                  display: "inline-block",
+                                  color: "black",
+                                }}
+                              >
+                                {letter}
+                              </div>
+                            );
+                          }
+                        })}
+                      </div>
+                    );
+                  }
+                })}
+              </CardText>
+            </CardBody>
+          </Card>
         </div>
-      )}
+        <br />
+        <div className="answerwithkeyboard">
+          <div className="answervalue"> {AnswerValue}</div> <br />
+          {PreviousGuesses.length >= 5 ||
+          HistoricalWoman == null ||
+          HistoricalWoman.lastname.toUpperCase() ===
+            PreviousGuesses[PreviousGuesses.length - 1] ? null : (
+            <div className="keyboard">
+              <div className="keyboardline">
+                <LetterButton
+                  letter="Q"
+                  onClick={() => UpdateAnswer([...AnswerValue, "Q"])}
+                />
+
+                <LetterButton
+                  letter="W"
+                  onClick={() => UpdateAnswer([...AnswerValue, "W"])}
+                />
+
+                <LetterButton
+                  letter="E"
+                  onClick={() => UpdateAnswer([...AnswerValue, "E"])}
+                />
+                <LetterButton
+                  letter="R"
+                  onClick={() => UpdateAnswer([...AnswerValue, "R"])}
+                />
+                <LetterButton
+                  letter="T"
+                  onClick={() => UpdateAnswer([...AnswerValue, "T"])}
+                />
+                <LetterButton
+                  letter="Y"
+                  onClick={() => UpdateAnswer([...AnswerValue, "Y"])}
+                />
+                <LetterButton
+                  letter="U"
+                  onClick={() => UpdateAnswer([...AnswerValue, "U"])}
+                />
+                <LetterButton
+                  letter="I"
+                  onClick={() => UpdateAnswer([...AnswerValue, "I"])}
+                />
+                <LetterButton
+                  letter="O"
+                  onClick={() => UpdateAnswer([...AnswerValue, "O"])}
+                />
+                <LetterButton
+                  letter="P"
+                  onClick={() => UpdateAnswer([...AnswerValue, "P"])}
+                />
+                <BackspaceButton
+                  message="Backspace"
+                  onClick={() => UpdateAnswer(AnswerValue.slice(0, -1))}
+                />
+              </div>
+
+              <div className="keyboardline">
+                <LetterButton
+                  letter="A"
+                  onClick={() => UpdateAnswer([...AnswerValue, "A"])}
+                />
+                <LetterButton
+                  letter="S"
+                  onClick={() => UpdateAnswer([...AnswerValue, "S"])}
+                />
+                <LetterButton
+                  letter="D"
+                  onClick={() => UpdateAnswer([...AnswerValue, "D"])}
+                />
+                <LetterButton
+                  letter="F"
+                  onClick={() => UpdateAnswer([...AnswerValue, "F"])}
+                />
+                <LetterButton
+                  letter="G"
+                  onClick={() => UpdateAnswer([...AnswerValue, "G"])}
+                />
+
+                <LetterButton
+                  letter="H"
+                  onClick={() => UpdateAnswer([...AnswerValue, "H"])}
+                />
+                <LetterButton
+                  letter="J"
+                  onClick={() => UpdateAnswer([...AnswerValue, "J"])}
+                />
+
+                <LetterButton
+                  letter="K"
+                  onClick={() => UpdateAnswer([...AnswerValue, "K"])}
+                />
+
+                <LetterButton
+                  letter="L"
+                  onClick={() => UpdateAnswer([...AnswerValue, "L"])}
+                />
+                <LetterButton
+                  letter="'"
+                  onClick={() => UpdateAnswer([...AnswerValue, "'"])}
+                />
+                <br />
+              </div>
+              <div className="keyboardline">
+                <LetterButton
+                  letter="Z"
+                  onClick={() => UpdateAnswer([...AnswerValue, "Z"])}
+                />
+                <LetterButton
+                  letter="X"
+                  onClick={() => UpdateAnswer([...AnswerValue, "X"])}
+                />
+                <LetterButton
+                  letter="C"
+                  onClick={() => UpdateAnswer([...AnswerValue, "C"])}
+                />
+                <LetterButton
+                  letter="V"
+                  onClick={() => UpdateAnswer([...AnswerValue, "V"])}
+                />
+
+                <LetterButton
+                  letter="B"
+                  onClick={() => UpdateAnswer([...AnswerValue, "B"])}
+                />
+                <LetterButton
+                  letter="N"
+                  onClick={() => UpdateAnswer([...AnswerValue, "N"])}
+                />
+                <LetterButton
+                  letter="M"
+                  onClick={() => UpdateAnswer([...AnswerValue, "M"])}
+                />
+                <LetterButton
+                  letter="-"
+                  onClick={() => UpdateAnswer([...AnswerValue, "-"])}
+                />
+
+                <br></br>
+
+                <GuessButton
+                  message="Guess Name"
+                  onClick={() => {
+                    UpdateLengthAlert(false);
+                    if (HistoricalWoman != null) {
+                      if (
+                        AnswerValue.length == HistoricalWoman.lastname.length
+                      ) {
+                        AddGuess([...PreviousGuesses, AnswerValue.join("")]);
+                        UpdateAnswer([]);
+                      } else {
+                        UpdateLengthAlert(true);
+                      }
+                    }
+                  }}
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
